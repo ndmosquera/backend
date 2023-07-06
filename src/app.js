e44272f511cda6431db2e605c9e31cf3754f9ab6
ndmosquera <nd.mosquera1581@gmail.com>
2023-07-06 15:48:08 -0500

    Desafio3-Mosquera

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import express from "express";
 import productManager from './productManager.js'
 
-
-
 const app = express();
+
+app.use(express.urlencoded({extended:true})) 
 
 const manager = new productManager('../data/products.json');
 
-app.get('/products', (req, res) => {
-    const products = manager.getProducts();
-    res.send(products);
+app.get('/products', async (req, res) => {
+    const products = await manager.getProducts();
+    const { limit } = req.query
+    res.send(
+        limit
+        ? products.slice(0, limit)
+        : products
+        );
 });
 
-app.get('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await manager.getProductById(Number(id));
+app.get('/products/:pid', async (req, res) => {
+    const { pid } = req.params;
+    const product = await manager.getProductById(Number(pid));
     res.send(product);
 });
 
