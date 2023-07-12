fab47c56f090f574bdc3afcc46a16c77dfa928cb
ndmosquera <nd.mosquera1581@gmail.com>
2023-07-11 21:23:50 -0500

    PrimeraPreEntrega-Mosquera

--- utils/GlobalConstants.mjs
+++ utils/GlobalConstants.mjs
@@ -1,11 +1,21 @@
+
+// Products fields
 export const PRODUCT_TITLE = 'title'
 export const PRODUCT_DESCRIPTION = 'description'
-export const PRODUCT_PRICE = 'price'
-export const PRODUCT_THUMBNAIL = 'thumbnail'
 export const PRODUCT_CODE = 'code'
+export const PRODUCT_PRICE = 'price'
+export const PRODUCT_STATUS = 'status'
 export const PRODUCT_STOCK = 'stock'
+export const PRODUCT_CATEGORY = 'category'
+export const PRODUCT_THUMBNAIL = 'thumbnail'
 export const PRODUCT_ID = 'id'
 
+// Carts fields
+export const CART_ID = 'id'
+export const CART_PRODUCTS = 'products'
+export const QUANTITY = "quantity"
 
-export const PATH_PRODUCTS_FILE = '../data/products.json'
-export const PATH_USERS_FILE = './data/users.json'
+export const PATH_PRODUCTS_FILE = '../src/data/products.json'
+export const PATH_CARTS_FILE = '../src/data/carts.json'
+export const PATH_USERS_FILE = '../src/data/users.json'
+
